55dba7d250265dfe30ce156e8a3141065933ad7d
Abhinay Jangde <abhinayjangde@gmail.com>
2025-02-11 19:19:48 +0530

    learning reactjs

---
+++ reactjs/crash/src/App.jsx
@@ -0,0 +1,15 @@
+
+import { useEffect, useState } from "react";
+import Counter from "../components/Counter";
+
+
+export default function App() {
+  
+  return (
+
+    <div>
+      <Counter/>
+    </div>
+
+  )
+}
