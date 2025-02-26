9b0228fbe43e6f2c30f5533c5760a9c2c32f5919
Abhinay Jangde <abhinayjangde@gmail.com>
2025-02-26 10:51:05 +0530

    randome quote generator project

---
+++ speedjs/work.js
@@ -0,0 +1,12 @@
+// const person = {
+//     username: "abhi",
+//     greet : function(){
+//         console.log(`hello, ${this.username}`)
+//     }
+// }
+
+// console.log("hi")
+// setTimeout(person.greet.bind(person), 5 * 1000)
+// console.log("bye")
+
+
