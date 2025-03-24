2ebbe313f8139b5e21ed494ee8653a4800557a01
Abhinay Jangde <abhinayjangde@gmail.com>
2025-03-24 22:21:02 +0530

    24-March Node internals and backend database

--- 80-20/this.js
+++ 80-20/this.js
@@ -1,19 +1,4 @@
-class Header {
-    constructor() {
-      this.color = "Red";
-    }
-  
-  //Arrow function:
-    changeColor = () => {
-      document.getElementById("id1").innerHTML += this;
-    }
-  }
-  
-  const myheader = new Header();
-  
-  
-  //The window object calls the function:
-  window.addEventListener("load", myheader.changeColor);
-  
-  //A button object calls the function:
-  document.getElementById("btn").addEventListener("click", myheader.changeColor);
+const books = ['math', 'science', 'history', 'english']
+// books.sort()
+const res = books.findIndex('history')
+console.log(res)
