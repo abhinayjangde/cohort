f0c6031eeda0f19e436a63f081bd097542197233
Abhinay Jangde <abhinayjangde@gmail.com>
2025-05-18 20:07:11 +0530

    learned about debounce

--- react-machine-coding/src/App.jsx
+++ react-machine-coding/src/App.jsx
@@ -1,10 +1,17 @@
 
 import CountryTask from './components/CountryTask'
+import Header from './components/Header'
+import ImageSlider from './components/ImageSlider'
+import StateVariables from './components/StateVariables'
+
 export default function App(){
 
   return (
     <>
-    <CountryTask/>
+    {/* <CountryTask/> */}
+    <Header title="React Interview" bg="red" />
+    {/* <StateVariables name="Abhi" /> */}
+    <ImageSlider/>
     </>
   )
 }
