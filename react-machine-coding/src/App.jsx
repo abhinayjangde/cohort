3c666d220042079ea0f8f98fb3556c600c4f7f1e
Abhinay Jangde <abhinayjangde@gmail.com>
2025-05-23 22:26:33 +0530

    created todoapp in react

--- react-machine-coding/src/App.jsx
+++ react-machine-coding/src/App.jsx
@@ -4,6 +4,7 @@ import Header from './components/Header'
 import ImageSlider from './components/ImageSlider'
 import Signup from './components/SIgnup'
 import StateVariables from './components/StateVariables'
+import TodoApp from './components/TodoApp'
 
 export default function App(){
 
@@ -13,7 +14,8 @@ export default function App(){
     <Header title="React Interview" bg="red" />
     {/* <StateVariables name="Abhi" /> */}
     {/* <ImageSlider/> */}
-    <Signup/>
+    {/* <Signup/> */}
+    <TodoApp/>
     </>
   )
 }
